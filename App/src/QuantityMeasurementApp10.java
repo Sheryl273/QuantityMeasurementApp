e409e7dc8e6b0134f398e9d993bba39981245116
Sheryl Chauhan <sherylchauhan95@gmail.com>
2026-05-01 21:11:35 +0530

    Refactor QuantityMeasurementApp10 to include length

--- App/src/QuantityMeasurementApp10.java
+++ App/src/QuantityMeasurementApp10.java
@@ -1,21 +1,53 @@
-public class QuantityMeasurementApp10{
+public class QuantityMeasurementApp10 {
 
     public static void main(String[] args) {
 
-        QuantityWeight kg = new QuantityWeight(1.0, WeightUnit.KILOGRAM);
-        QuantityWeight gram = new QuantityWeight(1000.0, WeightUnit.GRAM);
-        QuantityWeight pound = new QuantityWeight(2.20462, WeightUnit.POUND);
+        Quantity<LengthUnit> feet =
+            new Quantity<>(1.0, LengthUnit.FEET);
 
-        System.out.println("Equality:");
-        System.out.println(kg.equals(gram));
-        System.out.println(kg.equals(pound));
+        Quantity<LengthUnit> inches =
+            new Quantity<>(12.0, LengthUnit.INCHES);
 
-        System.out.println("\nConversion:");
-        System.out.println(kg.convertTo(WeightUnit.GRAM));
-        System.out.println(gram.convertTo(WeightUnit.POUND));
+        Quantity<WeightUnit> kg =
+            new Quantity<>(1.0, WeightUnit.KILOGRAM);
 
-        System.out.println("\nAddition:");
-        System.out.println(kg.add(gram));
-        System.out.println(kg.add(gram, WeightUnit.GRAM));
+        Quantity<WeightUnit> gram =
+            new Quantity<>(1000.0, WeightUnit.GRAM);
+
+        demonstrateEquality(feet, inches);
+        demonstrateConversion(feet, LengthUnit.INCHES);
+        demonstrateAddition(feet, inches, LengthUnit.FEET);
+
+        demonstrateEquality(kg, gram);
+        demonstrateConversion(kg, WeightUnit.GRAM);
+        demonstrateAddition(kg, gram, WeightUnit.KILOGRAM);
+    }
+
+    private static <U extends IMeasurable>
+    void demonstrateEquality(
+        Quantity<U> first,
+        Quantity<U> second) {
+
+        System.out.println(first + " equals " + second +
+            " -> " + first.equals(second));
+    }
+
+    private static <U extends IMeasurable>
+    void demonstrateConversion(
+        Quantity<U> quantity,
+        U targetUnit) {
+
+        System.out.println(quantity + " converts to " +
+            quantity.convertTo(targetUnit));
+    }
+
+    private static <U extends IMeasurable>
+    void demonstrateAddition(
+        Quantity<U> first,
+        Quantity<U> second,
+        U targetUnit) {
+
+        System.out.println(first + " + " + second +
+            " = " + first.add(second, targetUnit));
     }
 }
